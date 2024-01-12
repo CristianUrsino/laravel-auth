7dfc594c61a1c48b71021d8f11a3b1f1d6a6d01b
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-01-12 18:12:59 +0100

    added project create

---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+@section('content')
+    <section class="container">
+       <h1>project create</h1>
+
+       <form action="{{route('admin.projects.store')}}">
+
+            @csrf
+
+            <div class="mb-3">
+                <label for="name">Name</label>
+                <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name" value="{{ old('name') }}">
+                @error('name')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            
+
+            <div class="mb-3">
+                <label for="description">description</label>
+                <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" cols="30" rows="10"></textarea>
+                @error('description')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="repository_link">Repository link</label>
+                <input type="url" class="form-control @error('repository_link') is-invalid @enderror" name="repository_link" id="repository_link">
+                @error('repository_link')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="completion_date">completion_date</label>
+                <input type="url" class="form-control @error('completion_date') is-invalid @enderror" name="completion_date" id="completion_date">
+                @error('completion_date')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="project_status">project_status</label>
+                <select name="project_status" id="project_status">
+                    <option value="">all</option>
+                    <option value="active">active</option>
+                    <option value="completed">completed</option>
+                    <option value="paused">paused</option>
+                </select>
+                @error('project_status')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div>
+                <button type="submit" class="btn">Invia</button>
+                <button type="reset" class="btn">Reset</button>
+            </div>
+
+        </form>  
+
+    </section>
+@endsection
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts/app');
 @section('content')
     <h1>index project</h1>
+    <div class="mb-3">
+        <a href="{{route('admin.projects.create')}}">Aggiungi progetto</a>
+    </div>
     @foreach ($projects as $project)
         <div>
             <a href="{{route('admin.projects.show',$project->id)}}">{{$project->name}}</a>
