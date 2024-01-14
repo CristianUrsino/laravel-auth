0315c440b338694d8f1d8e24c6c7f7ff68e023ee
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-01-14 17:06:27 +0100

    added new dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,6 @@
     <section class="container">
         <h1>HOME</h1>
         <div><a href="{{route('admin.projects.index')}}">projects</a></div>
+        <div><a href="{{route('admin.dashboard')}}">dashboard</a></div>
     </section>
 @endsection
